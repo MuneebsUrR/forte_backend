6006562fa2b3c008d8774fcf3fae63775400ba41
Muneeb Ur Rehman <muneebsurrehman@gmail.com>
2024-07-11 19:13:43 +0500

    fetching all the questions accroding to each topic

--- Controllers/scheduleQPController.js
+++ Controllers/scheduleQPController.js
@@ -1,27 +1,54 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
-const  getScheduleQP = async (req, res) => {
+const getScheduleQP = async (req, res) => {
 
     try {
         //selecting a random qp_id from rqp_specification
         const randomQP_IDResult = await prisma.$queryRaw`SELECT QP_ID FROM rqp_specification ORDER BY RAND() LIMIT 1`;
-       
+
         if (randomQP_IDResult.length === 0) {
             throw new Error('No records found in the table.');
         }
 
-        
+
         const randomQP_ID = randomQP_IDResult[0].QP_ID;
 
         //fetching the record according to the random qp_id
         const record = await prisma.rqp_specification.findMany({
             where: {
-                QP_ID: randomQP_ID,
+                QP_ID: 811223,
             },
         });
 
-        res.status(200).send({ success: true, randomQP_ID, record });
+        //fetching questions according to sa_id from record
+        let all_questions = [];
+        for (let i = 0; i < record.length; i++) {
+
+            const sa_id = record[i].SA_ID;
+            const questions = await prisma.question.findMany({
+                where: {
+                    SA_ID: sa_id,
+                }
+            })
+
+            subject_name = record[i].NODE_NAME;
+            noq = record[i].NOQ;
+            wtg = record[i].WTG;
+            timeallocated = record[i].TIME_ALLOCATED;
+            isNegativeMarking = record[i].IS_NEGATIVE_MARKING;
+            all_questions.push({
+                sa_id,
+                subject_name,
+                noq,
+                wtg,
+                timeallocated,
+                isNegativeMarking,
+                questions
+            });
+        }
+
+        res.status(200).send({ success: true, randomQP_ID, all_questions });
 
     } catch (error) {
         console.log("Error fetching scheduleQP:", error);
@@ -29,6 +56,6 @@ const  getScheduleQP = async (req, res) => {
     }
 }
 
-module.exports={
+module.exports = {
     getScheduleQP,
 }
